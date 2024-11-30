dd9e5e85a8a015bc6a611154cd441b2e216f4913
Carol Tagliaferro <caroltagliaferro1@gmail.com>
2024-11-30 13:40:39 -0300

    Página de log in e sign up

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,21 @@
+// Para alternar entre login e cadastro
+
+document.getElementById('switch-to-signup').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.getElementById('login-form').classList.add('hidden');
+    document.getElementById('signup-form').classList.remove('hidden');
+});
+
+document.getElementById('switch-to-login').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.getElementById('signup-form').classList.add('hidden');
+    document.getElementById('login-form').classList.remove('hidden');
+});
+
+//Manda pra home
+
+document.getElementById('backhome-btn').addEventListener('click', function () {
+    window.location.href = '/index.html'; //eai kk tem q substituir pelo end certo
+});
+
+//Manda pro login
